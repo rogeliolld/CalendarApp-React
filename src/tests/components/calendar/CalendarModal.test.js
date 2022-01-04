f99b745c388f4183d162e0e0a71b0134bf222b27
Rogelio Llerena <rogelio.llerena@ceiba.com.co>
2022-01-04 10:35:28 -0500

    Test en 'CalendarModal'

--- src/tests/components/calendar/CalendarModal.test.js
+++ src/tests/components/calendar/CalendarModal.test.js
@@ -7,14 +7,23 @@ import thunk from 'redux-thunk';
 
 import '@testing-library/jest-dom';
 import { CalendarModal } from '../../../components/calendar/CalendarModal';
+import { eventClearActiveEvent, eventStartUpdate, eventStartAddNew } from '../../../actions/events';
+import { act } from '@testing-library/react';
+import Swal from 'sweetalert2';
 
 
-// jest.mock('../../../actions/events', ()=>({
-//     eventSetActive: jest.fn(),
-//     eventStartloading: jest.fn(),
+
+jest.mock('../../../actions/events', ()=>({
+    eventStartUpdate: jest.fn(),
+    eventClearActiveEvent: jest.fn(),
+    eventStartAddNew: jest.fn()
+}));
+
+jest.mock('sweetalert2', ()=>({
+    fire: jest.fn(),
+}));
 
 
-// }))
 
 const middlewares = [thunk];
 const mockStore = configureStore( middlewares );
@@ -54,17 +63,111 @@ const wrapper= mount(
 )
 
 describe('Pruebas en <CalendarModal/>', () => {
+
+    beforeEach(()=>{
+        jest.clearAllMocks()
+    })
     
     test('debe mostrar el Modal', () => {
-    
         // expect(wrapper.find('.modal').exists()).toBe(true);
         expect(wrapper.find('Modal').prop('isOpen')).toBe(true);
+    });
+
+    test('debe de llamar la acción de actualizar y cerrar el modal', () => {
         
+        wrapper.find('form').simulate('submit',{
+            preventDefault(){}
+        });
+
+        expect( eventStartUpdate ).toHaveBeenCalledWith(initState.calendar.activeEvent);
+        expect( eventClearActiveEvent ).toHaveBeenCalled();
+
+
+    });
+    
+    test('debe de mostar error si falta el titulo', () => {
+        
+        wrapper.find('form').simulate('submit',{
+            preventDefault(){}
+        });
+
+        expect( wrapper.find('input[name="title"]').hasClass('is-invalid') ).toBe(true);
+
+    });
+    
+    test('debe de crear un nuevo Evento', () => {
+       
+
+        const initState = {
+            calendar:{
+                events: [],
+                activeEvent: null
+            },
+            auth:{
+                uid: '123',
+                name: 'Juan'
+            },
+            ui:{
+                modalOpen: true
+            }
+        };
+        
+        const store = mockStore(initState);
+        store.dispatch = jest.fn();
+        
+        const wrapper= mount(
+           <Provider store={ store}>
+        
+               <CalendarModal/>
+        
+           </Provider>
+        );
+
+        wrapper.find('input[name="title"]').simulate('change',{
+            target:{
+                name: 'title',
+                value: 'Hola pruebas'
+            }
+        });
+
+        wrapper.find('form').simulate('submit',{
+            preventDefault(){}
+        });
+
+        expect(eventStartAddNew).toHaveBeenLastCalledWith({
+            end: expect.anything(),
+            start: expect.anything(),
+            title: 'Hola pruebas',
+            notes: ''
+            
+        });
+
+        expect( eventClearActiveEvent ).toHaveBeenCalled();
 
         
     });
 
+    test('debe de validar las fechas', () => {
+        
+        wrapper.find('input[name="title"]').simulate('change',{
+            target:{
+                name: 'title',
+                value: 'Hola pruebas'
+            }
+        });
 
+        const hoy = new Date();
+
+        act(()=>{
+            wrapper.find('DateTimePicker').at(1).prop('onChange')(hoy);
+        });
+
+        wrapper.find('form').simulate('submit',{
+            preventDefault(){}
+        });
+
+        expect( Swal.fire ).toHaveBeenCalledWith('Error!', 'La fecha fin debe de ser mayor a la fecha de inicio', 'error');
+
+    });
     
-
 })
